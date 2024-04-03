488299295bfd23283a4dee232cf7e020e7b567ab
r-clarissa <cgrodriguez@up.edu.ph>
2024-04-03 21:51:57 +0800

    mycatalog styling

--- lib/screen/MyCatalog.dart
+++ lib/screen/MyCatalog.dart
@@ -5,7 +5,7 @@ import "package:provider/provider.dart";
 import '../provider/shoppingcart_provider.dart';
 
 class MyCatalog extends StatefulWidget {
-  const MyCatalog({super.key});
+  const MyCatalog({Key? key}) : super(key: key);
 
   @override
   State<MyCatalog> createState() => _MyCatalogState();
@@ -22,36 +22,53 @@ class _MyCatalogState extends State<MyCatalog> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("My Catalog")),
-        body: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              // display the product list
-              return ListTile(
+      appBar: AppBar(
+        title: Center(child: const Text("My Catalog")),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          // display the product list
+          return Column(
+            children: [
+              ListTile(
                 leading: const Icon(Icons.star),
-                title: Text(
-                    "${productsCatalog[index].name} - ${productsCatalog[index].price}"),
-                // add item button
-                trailing: TextButton(
-                  child: const Text("Add"),
-                  onPressed: () {
-                    context
-                        .read<ShoppingCart>()
-                        .addItem(productsCatalog[index]);
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text("${productsCatalog[index].name} added!"),
-                      duration: const Duration(seconds: 1, milliseconds: 100),
-                    ));
-                  },
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(productsCatalog[index].name),
+                        Text("Price: ${productsCatalog[index].price}",
+                          style: TextStyle(fontSize: 14, color: Colors.grey,)),
+                      ],
+                    ),
+                    TextButton(
+                      child: const Text("+ Add"),
+                      onPressed: () {
+                        context.read<ShoppingCart>().addItem(productsCatalog[index]);
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("${productsCatalog[index].name} added!"),
+                          duration: const Duration(seconds: 1, milliseconds: 100),
+                        ));
+                      },
+                    ),
+                  ],
                 ),
-              );
-            },
-            itemCount: productsCatalog.length),
-        // floating shopping cart button
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.shopping_cart),
-          onPressed: () {
-            Navigator.pushNamed(context, "/cart");
-          },
-        ));
+              ),
+              Divider()
+            ],
+          );
+        },
+        itemCount: productsCatalog.length,
+      ),
+      // floating shopping cart button
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.shopping_cart),
+        onPressed: () {
+          Navigator.pushNamed(context, "/cart");
+        },
+      ),
+    );
   }
 }
